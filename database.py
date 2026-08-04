4d868bc5b2d0f31e9e946e4ab6bfbd86dc3682c5
Sadashiv Karhale <karhalesadashiv@gmail.com>
2026-08-04 18:28:30 +0530

    Remove hardcoded database credentials

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Change these according to your PostgreSQL setup
-DATABASE_URL = "postgresql://postgres:pune%40123@localhost:5432/speech_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
